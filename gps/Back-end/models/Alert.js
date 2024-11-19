6b89a88e7a530ecc87d55db79a8b01ca8535668c
JuanUribe7 <tu_email@ejemplo.com>
2024-11-19 17:14:34 -0500

    ask

---
+++ gps/Back-end/models/Alert.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const alertSchema = new mongoose.Schema({
+    imei: { type: String, required: true },
+    alertName: { type: String, required: true },
+    alertTime: { type: Date, required: true }
+}, { minimize: false });
+
+module.exports = mongoose.model('Alert', alertSchema);
