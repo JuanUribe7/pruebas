c64b9e98f1067429715cf69889f3713af1d58832
JuanUribe7 <tu_email@ejemplo.com>
2024-11-23 05:01:32 -0500

    sdsd

--- gps/Back-end/server.js
+++ gps/Back-end/server.js
@@ -1,22 +1,20 @@
 const net = require('net');
-const axios = require('axios'); // Asegúrate de tener axios instalado e importado
+const axios = require('axios');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const path = require('path'); 
+const path = require('path');
 const authRoutes = require('./routes/auth');
 const deviceRoutes = require('./routes/devices');
-const Gt06 = require('./gt06'); // Asegúrate de tener el módulo Gt06
+const Gt06 = require('./gt06');
 const mqtt = require('mqtt');
-const notificationRoutes = require('./routes/notificaciones');
+const notificacionRoutes = require('./routes/notificaciones');
 const { WebSocketServer } = require('ws');
 
-
-
 const PORT = process.env.GT06_SERVER_PORT || 4000;
-const HTTP_PORT = process.env.HTTP_PORT || 80;
+const HTTP_PORT = process.env.HTTP_PORT || 8080; // Cambia el puerto a 8080
 const rootTopic = process.env.MQTT_ROOT_TOPIC || 'gt06';
 const brokerUrl = process.env.MQTT_BROKER_URL || '11ec3ffa829840c785105a23a3994db1.s1.eu.hivemq.cloud';
 const brokerPort = process.env.MQTT_BROKER_PORT || 1883;
@@ -33,7 +31,7 @@ const mqttClient = mqtt.connect({
     password: brokerPasswd
 });
 
-app.use(express.static(path.join(__dirname, 'dist' )));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Servidor TCP
 var tcpServer = net.createServer((client) => {
@@ -61,14 +59,14 @@ var tcpServer = net.createServer((client) => {
         }
         gt06.msgBuffer.forEach(async (msg) => {
             mqttClient.publish(rootTopic + '/' + gt06.imei + '/pos', JSON.stringify(msg));
-            
+
             // Preparar los datos para enviar a la ruta /update-from-gps
             if (gt06.event.string === 'location') {
                 const gpsTime = new Date(gt06.fixTime);
-            
+
                 // Convertir a la hora local
                 const localTime = new Date(gpsTime.toLocaleString('en-US', { timeZone: 'America/Bogota' }));
-                
+
                 // Formatear la hora local en ISO 8601
                 const localTimeISO = localTime.toISOString();
                 const deviceData = {
@@ -91,29 +89,29 @@ var tcpServer = net.createServer((client) => {
                     course: gt06.course,
                     fixTime: localTimeISO
                 };
-            
+
                 console.log(
                     gt06.terminalInfo ? gt06.terminalInfo.ignition : 'undefined',
                     gt06.terminalInfo ? gt06.terminalInfo.charging : 'undefined',
                     gt06.terminalInfo ? gt06.terminalInfo.gpsTracking : 'undefined',
                     gt06.terminalInfo ? gt06.terminalInfo.relayState : 'undefined'
                 );
-            
+
                 // Enviar los datos a la ruta /update-from-gps
                 try {
                     await axios.post(`http://3.12.147.103/devices/update-from-gps`, deviceData);
-                    await axios.post(`http://3.12.147.103/devices/save-history`, historyData);  
+                    await axios.post(`http://3.12.147.103/devices/save-history`, historyData);
 
                     console.log(`Datos enviados a /update-from-gps para IMEI: ${gt06.imei}`);
                 } catch (error) {
                     console.error('Error al enviar los datos:', error);
                 }
-              
+
             }
         });
         gt06.clearMsgBuffer();
     });
-}); 
+});
 
 // Inicia el servidor TCP en el puerto especificado
 tcpServer.listen(PORT, () => {
@@ -127,26 +125,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Conexión a MongoDB
 mongoose.connect('mongodb+srv://lospopulare:gps1234@gps.zgbl7.mongodb.net/proyecto?retryWrites=true&w=majority&appName=gps', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-.then(() => console.log('Conectado a MongoDB'))
-.catch(err => console.error('Error de conexión a MongoDB:', err));
+    .then(() => console.log('Conectado a MongoDB'))
+    .catch(err => console.error('Error de conexión a MongoDB:', err));
 
 // Rutas
 app.use('/auth', authRoutes);
 app.use('/devices', deviceRoutes);
-app.use('/notification', notificationRoutes);
+app.use('/notificaciones', notificacionRoutes);
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 // Inicia el servidor HTTP en el puerto especificado
-app.listen(HTTP_PORT, () => {
+const server = app.listen(HTTP_PORT, () => {
     console.log(`Servidor HTTP corriendo en http://localhost:${HTTP_PORT}`);
 });
 
-const wss = new WebSocketServer(3002);
+// Configuración del servidor WebSocket
+const wss = new WebSocketServer({ server });
 
 wss.on('connection', (ws) => {
     console.log('Cliente WebSocket conectado');
@@ -168,4 +167,6 @@ const enviarNotificacion = (notificacion) => {
         }
     });
 };
+
+// Exportar la función para que pueda ser utilizada en otros módulos
 module.exports = { enviarNotificacion };
